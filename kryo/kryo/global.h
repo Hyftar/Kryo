39ca5b9bf51344a96136047cfe69a8343c3ece17
Jérémie Pratte-Bernard <LouisTakePILLz@users.noreply.github.com>
2015-10-03 17:21:39 -0400

    Mise à jour des directives d'inclusion

--- kryo/kryo/global.h
+++ kryo/kryo/global.h
@@ -2,7 +2,13 @@
 #define KRYO_GLOBAL_H
 
 #include "system.h"
+#include <stdint.h>
 #include <assert.h>
+
+#include <SFML/Window.hpp>
+
+#include <gl/GL.h>
+#include <gl/GLU.h>
 
 #define KRYO_BEGIN_NAMESPACE namespace Kryo {
 #define KRYO_END_NAMESPACE }
--- kryo/kryo/system.h
+++ kryo/kryo/system.h
@@ -1,6 +1,20 @@
 #ifndef KRYO_SYSTEM_H
 #define KRYO_SYSTEM_H
 
+#define WIN32_LEAN_AND_MEAN
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include <iostream>
 #include <string>
 
